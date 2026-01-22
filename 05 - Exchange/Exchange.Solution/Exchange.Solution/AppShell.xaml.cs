25f8b234bf9e155d49a3641d28e79cc07e6e28ad
binaj <b1csoben@vasvari.org>
2026-01-22 07:48:20 +0100

    solution created

---
+++ 05 - Exchange/Exchange.Solution/Exchange.Solution/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Exchange.Solution
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 05 - Exchange/Exchange.Solution/Exchange.Solution/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Exchange.Solution
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
