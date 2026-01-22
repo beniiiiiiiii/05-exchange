668132d0cf33738b1e489fea55e682a482cab43c
binaj <b1csoben@vasvari.org>
2026-01-22 09:16:51 +0100

    domain kesz

---
+++ 05 - Exchange/Exchange.Solution/Solution.Domain/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Solution.Domain.Database;
+
+public sealed class ApplicationDbContext : IdentityDbContext<UserEntity, IdentityRole<Guid>, Guid>
+{
+    public override DbSet<UserEntity> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ConfigureUser();
+    }
+
+
+}
---
+++ 05 - Exchange/Exchange.Solution/Solution.Domain/Database/Builders/UserEntityModelBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution.Domain.Database.Builders;
+
+internal static class UserEntityModelBuilder
+{
+    public static void ConfigureUser(this ModelBuilder builder)
+    {
+        builder.Entity<UserEntity>(entity =>
+        {
+            entity.ToTable("Users");
+
+            entity.HasIndex(e => e.Id)
+                  .IsUnique();
+            
+            entity.Property(e => e.FullName)
+                .HasColumnName("FullName")
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(e => e.RegisteredAtUtc)
+                .HasColumnName("RegisteredAtUtc");
+        });
+    }
+}
