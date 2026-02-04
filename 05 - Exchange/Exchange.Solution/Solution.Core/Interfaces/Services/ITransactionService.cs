d108c98bee9f768a99088531112ef0b461293003
binaj <b1csoben@vasvari.org>
2026-02-04 09:17:02 +0100

    Transaction interface

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Interfaces/Services/ITransactionService.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using Solution.Core.Models.Requests;
+
+namespace Solution.Core.Interfaces.Services;
+
+public interface ITransactionService
+{
+    Task<ErrorOr<TransactionResponse>> CreateBuyTransactionAsync(CreateTransactionRequest request);
+    Task<ErrorOr<TransactionResponse>> CreateSellTransactionAsynx(CreateTransactionRequest request);
+    Task<ErrorOr<TransactionResponse>> GetTransactionAsync(DateOnly? date, Currency? currency, TransactionType? type);
+    Task<ErrorOr<TransactionResponse>> GetTransactionByIdAsync(int id);
+}
