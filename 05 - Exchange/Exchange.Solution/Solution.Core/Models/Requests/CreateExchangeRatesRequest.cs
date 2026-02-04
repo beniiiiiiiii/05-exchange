bad9f915cc312086a206548a42c507afc6f1b428
jamaikaivasutas <captainmine0@gmail.com>
2026-02-04 08:15:54 +0100

    feat: added CreateExchangeRatesModel

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/Requests/CreateExchangeRatesRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace Solution.Core.Models.Requests;
+
+public class CreateExchangeRatesRequest
+{
+    [JsonPropertyName("date")]
+    public DateOnly Date {  get; set; }
+
+    [JsonPropertyName("usdBuyRate")]
+    public decimal UsdBuyRate { get; set; }
+
+    [JsonPropertyName("usdSellRate")]
+    public decimal UsdSellRate { get; set; }
+
+    [JsonPropertyName("gbpBuyRate")]
+    public decimal gbpBuyRate { get; set; }
+
+    [JsonPropertyName("gbpSellRate")]
+    public decimal gbpSellRate { get; set; }
+
+    [JsonPropertyName("chfBuyRate")]
+    public decimal chfBuyRate { get; set; }
+
+    [JsonPropertyName("chfSellRate")]
+    public decimal chfSellRate { get; set; }
+}
