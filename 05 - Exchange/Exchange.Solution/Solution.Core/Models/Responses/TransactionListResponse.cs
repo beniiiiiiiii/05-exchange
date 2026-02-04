d025a4927be65bd6eb1777df29829b2a6783799a
jamaikaivasutas <captainmine0@gmail.com>
2026-02-04 09:02:29 +0100

    feat: added transaction related responses

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/Responses/TransactionListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution.Core.Models.Response;
+
+public class TransactionListResponse
+{
+    [JsonPropertyName("transactions")]
+    public List<TransactionResponse> Transactions { get; set; } = new();
+
+    [JsonPropertyName("totalCount")]
+    public int TotalCount { get; set; }
+}
---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/Responses/TransactionResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace Solution.Core.Models.Responses;
+
+public class TransactionResponse
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("currency")]
+    public string Currency { get; set; }
+
+    [JsonPropertyName("foreignAmount")]
+    public decimal ForeignAmount { get; set; }
+
+    [JsonPropertyName("hufAmount")]
+    public decimal HufAmount { get; set; }
+
+    [JsonPropertyName("AppliedRate")]
+    public decimal AppliedRate { get; set; }
+
+    [JsonPropertyName("customerName")]
+    public string CustomerName { get; set; }
+
+    [JsonPropertyName("customerIdType")] 
+    public string CustomerIdType { get; set; }
+
+    [JsonPropertyName("customerIdNumber")]
+    public string CustomerIdNumber { get; set; }
+
+    [JsonPropertyName("transactionDate")]
+    public DateTime TransactionDate { get; set; }
+
+    [JsonPropertyName("processedBy")]
+    public string ProcessedBy { get; set; }
+}
