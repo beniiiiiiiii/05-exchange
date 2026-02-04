3c541b0b572917a85a6c0de2181709229a308fc6
jamaikaivasutas <captainmine0@gmail.com>
2026-02-04 08:18:28 +0100

    feat: added UpdateExchangeRateRequest

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/UpdateExchangeRateRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Solution.Core.Models.Request;
+
+public class UpdateExchangeRateRequest
+{
+    [JsonPropertyName("currency")]
+    public Currency Currency { get; set; }
+
+    [JsonPropertyName("buyRate")]
+    public decimal BuyRate { get; set; }
+
+    [JsonPropertyName("sellRate")]
+    public decimal SellRate { get; set; }
+}
