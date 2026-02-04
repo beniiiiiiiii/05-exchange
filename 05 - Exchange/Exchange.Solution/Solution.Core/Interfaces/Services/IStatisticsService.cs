3209b0d09aba6d68b6b2ee5b736e452db87b00d7
binaj <b1csoben@vasvari.org>
2026-02-04 09:20:58 +0100

    Statistics interface

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Interfaces/Services/IStatisticsService.cs
@@ -0,0 +1,8 @@
+﻿namespace Solution.Core.Interfaces.Services;
+
+public interface IStatisticsService
+{
+    Task<ErrorOr<List<RateStatisticsResponse>>> GetRateStatisticsAsync(DateOnly startdDate, DateOnly endDate);
+    Task<ErrorOr<List<TransactionStatisticsResponse>>> GetTransactionStatisticsAsync(DateOnly startDate, DateOnly endDate);
+    Task<ErrorOr<SummaryStatisticsResponse>> GetSummaryAsync();
+}
