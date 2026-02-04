ad54fedf11353eb9712910c5bf8c1dc05d633763
jamaikaivasutas <captainmine0@gmail.com>
2026-02-04 08:22:24 +0100

    feat: added CreateTransactionRequest

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/Requests/CreateTransactionRequest.cs
@@ -0,0 +1,18 @@
+﻿using Solution.Domain.Enums;
+
+namespace Solution.Core.Models.Requests;
+
+public class CreateTransactionRequest
+{
+    [JsonPropertyName("currency")]
+    public Currency Currency { get; set; }
+
+    [JsonPropertyName("foreignAmount")]
+    public decimal ForeignAmount { get; set; }
+
+    [JsonPropertyName("customerName")]
+    public string CustomerName { get; set; }
+
+    [JsonPropertyName("customerIdType")]
+    public CustomerIdType CustomerIdType { get; set; }
+}
