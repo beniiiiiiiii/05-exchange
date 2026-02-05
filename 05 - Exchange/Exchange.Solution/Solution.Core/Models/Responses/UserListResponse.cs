557434b3c2a45cdf47ede8cad7432929ebd15678
binaj <b1csoben@vasvari.org>
2026-02-05 07:32:17 +0100

    UserListResponse

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Models/Responses/UserListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution.Core.Models.Responses;
+
+public class UserListResponse
+{
+    [JsonPropertyName("users")]
+    public List<UserResponesModel> Users { get; set; } = new();
+
+    [JsonPropertyName("totalCount")]
+    public int TotalCount { get; set; }
+}
