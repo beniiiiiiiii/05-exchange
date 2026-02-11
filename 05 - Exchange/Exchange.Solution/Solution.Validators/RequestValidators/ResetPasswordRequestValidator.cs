012126ff171ad768fec13413253fd14a4212e248
binaj <b1csoben@vasvari.org>
2026-02-11 08:00:45 +0100

    ResetPasswordRequestValidator made

---
+++ 05 - Exchange/Exchange.Solution/Solution.Validators/RequestValidators/ResetPasswordRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace Solution.Validators.RequestValidators;
+
+public class ResetPasswordRequestValidator: AbstractValidator<ResetPasswordRequest>
+{
+    public ResetPasswordRequestValidator()
+    {
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .WithMessage("New password is required.")
+            .MinimumLength(8)
+            .WithMessage("New password must be at least 8 characters long.")
+    }
+}
