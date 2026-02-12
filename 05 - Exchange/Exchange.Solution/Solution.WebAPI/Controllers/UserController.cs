59ac2f4d2e2d14dff1e4cdc48d4a4bf669b73364
binaj <b1csoben@vasvari.org>
2026-02-12 09:05:03 +0100

    user controller

--- 05 - Exchange/Exchange.Solution/Solution.WebAPI/Controllers/UserController.cs
+++ 05 - Exchange/Exchange.Solution/Solution.WebAPI/Controllers/UserController.cs
@@ -1,20 +1,84 @@
-﻿namespace Solution.WebAPI.Controllers;
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Solution.Core.Models.Requests;
+using Solution.Core.Models.Responses;
+using System.Net;
+using System.Security.Claims;
 
-[ApiController]
-[ProducesResponseType(statusCode: 400, type: typeof(BadRequestObjectResult))]
-public class UserController(IUserService userService) : ControllerBase
+namespace Solution.WebAPI.Controllers;
+
+[Authorize(Roles = "Administrator")]
+[Route("Controller")]
+public class UserController(IUserManagementService userManagementService) : BaseController
 {
     [HttpGet]
-    [Route("api/users")]
-    [Authorize]
-    [ProducesResponseType(type: typeof(ICollection<UserModel>), statusCode: 200)]
-    [EndpointDescription("This endpoint will return all users from the database you hiki hoki hokker")]
-    public async Task<IActionResult> GetUsersAsync()
+    [ProducesResponseType(typeof(UserListResponse), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetAllUsersAsync()
     {
-        var result = await userService.GetAllUsers();
+        var result = await userManagementService.GetAllUsersAsync();
         return result.Match(
-            value => Ok(value),
-            errors => errors.ToProblemResult()
-            );
+            result => Ok(result),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(UserResponseModel), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetUserByIdAsync([FromRoute] string id)
+    {
+        var result = await userManagementService.GetUserByIdAsync(id);
+        return result.Match(
+            result => Ok(result),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(UserResponseModel), (int)HttpStatusCode.Created)]
+    public async Task<IActionResult> CreateUserAsync(
+        [FromBody][Required] CreateUserRequest request)
+    {
+        var result = await userManagementService.CreateUserAsync(request);
+        return result.Match(
+            result => CreatedAtAction(nameof(GetUserByIdAsync), new { id = result.Id }, result),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(UserResponseModel), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdateUserAsync(
+        [FromRoute] string id,
+        [FromBody][Required] UpdateUserRequest request)
+    {
+        var result = await userManagementService.UpdateUserAsync(id, request);
+        return result.Match(
+            result => Ok(result),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    public async Task<IActionResult> DeleteUserAsync([FromRoute] string id)
+    {
+        var currentUserId = User.FindFirstValue("uid");
+        var result = await userManagementService.DeleteUserAsync(id, currentUserId);
+        return result.Match(
+            _ => NoContent(),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpPost("{id}/reset-password")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    public async Task<IActionResult> ResetPasswordAsync(
+        [FromRoute] string id,
+        [FromBody][Required] ResetPasswordRequest request)
+    {
+        var result = await userManagementService.ResetPasswordAsync(id, request);
+        return result.Match(
+            _ => NoContent(),
+            errors => Problem(errors)
+        );
     }
 }
