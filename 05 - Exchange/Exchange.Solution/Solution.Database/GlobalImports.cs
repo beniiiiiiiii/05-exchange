8bbe37353bda03249ddbf366a6c246308934d8ba
jamaikaivasutas <b1duzale@vasvari.org>
2026-02-18 09:31:39 +0100

    chore: minor changes to dependancies and typo fixes

--- 05 - Exchange/Exchange.Solution/Solution.Database/GlobalImports.cs
+++ 05 - Exchange/Exchange.Solution/Solution.Database/GlobalImports.cs
@@ -1,5 +1,4 @@
 ﻿global using Solution.Database.Enums;
-global using Solution.Domain.Database.Entities;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
