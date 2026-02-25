7a2f2ccea902f2d63a037935e9a6671da0a7835a
jamaikaivasutas <b1duzale@vasvari.org>
2026-02-25 08:03:54 +0100

    feat: added ErrorOrExtension, ISecurityService.

---
+++ 05 - Exchange/Exchange.Solution/Solution.Core/Interfaces/Services/ISecurityService.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Solution.Core.Interfaces.Services;
+
+public interface ISecurityService
+{
+    Task<ErrorOr<TokenResponseModel>> LoginAsync(LoginRequestModel model);
+    Task<ErrorOr<Success>> RegisterAsync(RegisterRequestModel model);
+}
