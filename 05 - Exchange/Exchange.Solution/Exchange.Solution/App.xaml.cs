7683591f73dbaca128dd4dbbedc7daa64544a7d7
Benedek Csombó-Varga <csombovargabenedek@gmail.com>
2026-03-04 00:54:50 +0100

    fixes

--- 05 - Exchange/Exchange.Solution/Exchange.Solution/App.xaml.cs
+++ 05 - Exchange/Exchange.Solution/Exchange.Solution/App.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace Exchange.Solution
+﻿namespace Solution.DesktopApp
 {
     public partial class App : Application
     {
+        private const string DefaultApiUrl = "https://localhost:7001";
+
         public App()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+
+            // Initialize API Client
+            var apiClient = IPlatformApplication.Current?.Services.GetService<IApiClient>();
+            if (apiClient != null)
+            {
+                apiClient.SetBaseAddress(DefaultApiUrl);
+                await apiClient.LoadStoredTokenAsync();
+            }
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
