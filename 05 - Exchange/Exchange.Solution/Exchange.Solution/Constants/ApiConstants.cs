8f073d5660c11962beb6b8cf254cfe1510a6c454
jamaikaivasutas <captainmine0@gmail.com>
2026-03-04 00:32:19 +0100

    feat: Added ApiConstants

---
+++ 05 - Exchange/Exchange.Solution/Exchange.Solution/Constants/ApiConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Exchange.Solution.Constants;
+
+public static class ApiConstants
+{
+    public static class Endpoints
+    {
+        public const string ExchangeRate = "exchangerate";
+        public const string ExchangeRateToday = "exchangerate/today";
+        public const string Transaction = "transaction";
+        public const string TransactionBuy = "transaction/buy";
+        public const string TransactionSell = "transaction/sell";
+        public const string StatisticsSummary = "statistics/summary";
+        public const string StatisticsRates = "statistics/rates";
+        public const string StatisticsTransactions = "statistics/transactions";
+        public const string User = "user";
+    }
+}
